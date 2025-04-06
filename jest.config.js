3b188415d8939070aacac7d5ccc7e7535764bf70
telkcom2 <telkcom2@gmail.com>
2025-04-06 22:21:03 +0200

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
